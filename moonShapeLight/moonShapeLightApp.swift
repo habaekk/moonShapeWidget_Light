4baf26395714fb40732613705241b6bfbd921471
habaek <haback1493@gmail.com>
2023-04-19 03:19:34 +0900

    initial commit

---
+++ moonShapeLight/moonShapeLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  moonShapeLightApp.swift
+//  moonShapeLight
+//
+//  Created by macbook1 on 2023/04/19.
+//
+
+import SwiftUI
+
+@main
+struct moonShapeLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moonShpaeWidgetLight/moonShpaeWidgetLightBundle.swift
@@ -0,0 +1,17 @@
+//
+//  moonShpaeWidgetLightBundle.swift
+//  moonShpaeWidgetLight
+//
+//  Created by macbook1 on 2023/04/19.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct moonShpaeWidgetLightBundle: WidgetBundle {
+    var body: some Widget {
+        moonShpaeWidgetLight()
+        moonShpaeWidgetLightLiveActivity()
+    }
+}
